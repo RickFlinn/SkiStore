21c0f421f8cc6b118a252d4ad475958f7fe016cd
Richard Flinn <mriodine@gmail.com>
2019-02-19 16:04:04 -0800

    Built app skeleton

--- SkiStore/SkiStore/Startup.cs
+++ SkiStore/SkiStore/Startup.cs
@@ -14,7 +14,11 @@ namespace SkiStore
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            services.AddMvc();
+
+            //services.AddDbContext<SkiStoreDbContext>(options =>
+            //  options. SOME STUFF );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +29,12 @@ namespace SkiStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+
+          
         }
     }
 }
