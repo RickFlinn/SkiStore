afb46e373afd6cfce5793f4fab2530bd4236545e
Richard Flinn <mriodine@gmail.com>
2019-02-22 12:12:29 -0800

    Added user login controller actions, view model and page.

---
+++ SkiStore/SkiStore/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkiStore.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public string ErrorMessage { get; set; }
+        public string AlertMessage { get; set; }
+    }
+}
