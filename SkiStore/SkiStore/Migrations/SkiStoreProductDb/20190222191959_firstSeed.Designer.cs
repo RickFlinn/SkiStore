46fa4853bb2cf28109819e51efb6f2fa76c690ad
andrewquamme <andrewquamme@gmail.com>
2019-02-22 11:20:42 -0800

    seed product DB

--- SkiStore/SkiStore/Migrations/SkiStoreProductDb/20190222191959_firstSeed.Designer.cs
+++ SkiStore/SkiStore/Migrations/SkiStoreProductDb/20190222191959_firstSeed.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkiStore.Data;
 
 namespace SkiStore.Migrations.SkiStoreProductDb
 {
     [DbContext(typeof(SkiStoreProductDbContext))]
-    partial class SkiStoreProductDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190222191959_firstSeed")]
+    partial class firstSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
