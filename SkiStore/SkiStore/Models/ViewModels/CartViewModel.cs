5a4ceb573fea4221b879a63e84260919d3336cb9
Richard Flinn <mriodine@gmail.com>
2019-02-25 12:24:03 -0800

    Revert "Revert "Added Cart Controller that requires "WaivedAdult" policy, and associated view model, CartItem model, and ICartManager interface.""
    This reverts commit 17f437ca9fdbac7365290dc6d6e90761580933a2.

---
+++ SkiStore/SkiStore/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkiStore.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<CartItem> CartItems { get; set; }
+        public string User { get; set; }
+
+        
+        public Product Item { get; set; }
+        public int Quantity { get; set; }
+
+        public string AlertMessage { get; set; }
+        public string AlertSummary { get; set; }
+    }
+}
