6489a0eaa3169d827cd520920f16bb2672d85d24
Richard Flinn <mriodine@gmail.com>
2019-03-05 23:38:55 -0800

    Updated dbcontext with Orders.

--- SkiStore/SkiStore/Migrations/20190306071947_orders.Designer.cs
+++ SkiStore/SkiStore/Migrations/20190306071947_orders.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkiStore.Data;
 
 namespace SkiStore.Migrations
 {
     [DbContext(typeof(SkiStoreProductDbContext))]
-    partial class SkiStoreProductDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190306071947_orders")]
+    partial class orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
